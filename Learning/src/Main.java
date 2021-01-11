7a889a8c5a2c873c21e8a064074a64c35642e59f
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-11 18:20:05 +0300

    Initial commit in java learning

---
+++ Learning/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal",1,1,5,5);
+     //   Dog dog = new Dog("Chui",8,20,2,4,20,"silky");
+        //dog.move(8);
+        //dog.run();
+    }
+}
