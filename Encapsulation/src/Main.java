049b5d6774773c8b076f60a21af3e17988667bc7
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-11 22:31:10 +0300

    Enhanced Player Class to show encapsulation using the private access modifier

--- Encapsulation/src/Main.java
+++ Encapsulation/src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Player player = new Player();
-        player.name = "Tim";
-        player.health =  20;
-        player.weapon ="Sword";
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health =  20;
+//        player.weapon ="Sword";
+//
+//        int damage =10;
+//        player.loseHealth(damage);
+//        System.out.println(player.healthRemaining());
+//
+//        damage = 11;
+//        player.loseHealth(damage);
+//        System.out.println(player.healthRemaining());
 
-        int damage =10;
-        player.loseHealth(damage);
-        System.out.println(player.healthRemaining());
 
-        damage = 11;
-        player.loseHealth(damage);
-        System.out.println(player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("Kajuna",200,"Sword");
+        System.out.println("Initial health player "+player.getHealth());
     }
 }
