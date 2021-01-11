632794f7225e5a56b1b799c518c26efa439bdd41
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-11 18:32:49 +0300

    Testing the composition in the main class

--- Composition/src/Main.java
+++ Composition/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B","Dell", "240",dimensions);
+        Monitor theMonitor = new Monitor("27 inch Beast","Acer",27,new Resolution(2540,1440));
+        Motherboard motherboard = new Motherboard("BJ-200","Asus",4,6,"v2.44");
+        PC thePc = new PC(theCase,theMonitor,motherboard);
 
+        thePc.getMonitor().drawPixelAt(1500,1200,"Red");
+        thePc.getMotherboard().loadProgram("Wondows 1.0");
+        thePc.getTheCase().pressPowerButton();
     }
 }
