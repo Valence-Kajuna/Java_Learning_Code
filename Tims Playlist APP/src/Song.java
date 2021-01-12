7f2e32d9296a2c268dfca62bd12bb4a944c2162c
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-13 00:13:40 +0300

    Adding the functionality so as to be able to read the song name and duration

--- Tims Playlist APP/src/Song.java
+++ Tims Playlist APP/src/Song.java
@@ -6,4 +6,13 @@ public class Song {
         this.title = title;
         this.duration = duration;
     }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title +":" +this.duration;
+    }
 }
