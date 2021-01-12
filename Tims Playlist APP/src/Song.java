02c145f26864ecc283e184c54ebd864901c43598
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-13 00:09:30 +0300

    Setting up the constructor for the song class

--- Tims Playlist APP/src/Song.java
+++ Tims Playlist APP/src/Song.java
@@ -1,2 +1,9 @@
 public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
 }
