22f223d273b75174a28ee2cece03f2de67e07484
Valence Kajuna <valencekajuna@yahoo.com>
2021-01-12 19:52:30 +0300

    To show that Linked List in  Java is implemented as Doubly Linked list

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
     public static boolean addItem(String item, LinkedList<String> myList){
@@ -26,6 +27,52 @@ public class Main {
             System.out.println("Now visiting " +i.next());
         }
     }
+
+    //Code to show that the linked list in Java is implemented as the double linked list
+    public static void visit(LinkedList<String> cities){
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        ListIterator<String> i = cities.listIterator();
+        if (cities.isEmpty()){
+            System.out.println("There are no cities in your Region");
+        }else{
+            System.out.println("Now visiting "+ i.next());
+            printMenu();
+        }
+        while(!quit){
+            int action = scanner.nextInt();
+            switch(action){
+                case 0:
+                    System.out.println("Holiday is over 😁👋");
+                    quit  = true;
+                    break;
+
+                case 1:
+                    if(i.hasNext()){
+                        System.out.println("You are now visiting "+ i.next());
+                    }else{
+                        System.out.println("You have reached the end of the journey");
+                    }
+                    break;
+
+                case 2:
+                    if(i.hasPrevious()){
+                        System.out.println("You are now visiting "+ i.previous());
+                        break;
+                    }else{
+                        System.out.println("You are at your starting position");
+
+                    }
+                case 3:
+                    printMenu();
+                    break;
+            }
+        }
+    }
+    public static void printMenu(){
+        System.out.println("Available actions:\nPress:");
+        System.out.println("0: to quit \n 1:To move forward \n 2: To move backward \n 3:To see the menu again");
+    }
     public static void main(String[] args) {
         LinkedList<String> names = new LinkedList<String>();
         addItem("Bukoba",names);
@@ -33,8 +80,11 @@ public class Main {
         addItem("Karagwe",names);
         addItem("Biharamulo",names);
         addItem("Muleba",names);
-
         print(names);
+        System.out.println("");
+        System.out.println("=====================");
+        visit(names);
+
 
     }
 }
